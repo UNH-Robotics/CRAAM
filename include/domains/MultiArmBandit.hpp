b895daeac1a8cb44e0a066858dbe4007309f0f1a
Marek Petrik <mpetrik@cs.unh.edu>
2016-11-09 11:00:51 -0500

    added a simulator and fixed thompson sampling code

--- include/domains/MultiArmBandit.hpp
+++ include/domains/MultiArmBandit.hpp
@@ -1,19 +1,110 @@
 #pragma once
 
+#include <random>
+#include <tuple>
+#include <utility>
+
+using namespace std;
+
 namespace craam {
+namespace bandits{
 
-class MultiArmBandit {
+/**
+Models the evolution of the belief space, the reward probabilities depend on the state.
+This is *NOT* the simulator to be used for evaluation. The probability of a success for each arm depends 
+on the state.
+
+State:
+( Arm0 positive count, Arm0 negative count, Arm1 positive count, Arm1 negative count )
+
+Action: {0,1} = arm to pull
+
+Careful with parallelization; shared random 
+*/
+class MultiArmedBandit_BeliefSpace {
 public:
-    class State {
-        
-    };
-    
-    class Action {
-        
+    typedef tuple<long,long,long,long> State;
+    typedef int Action; // TODO: change it to enum class?
+
+    /** Constructor */
+    MultiArmedBandit(random_device::result_type seed = random_device{}): generator(seed) {};
+
+    /** The initial state in simulations */
+    State init_state(){return make_tuple(1,1,1,1)};
+
+    /** Probabilistic transition function */
+    pair<double,State> transition(State s, Action a){
+        assert(a == 0 || a == 1);
+
+        if(a == 0){
+            bernoulli_distribution dst(s[0]/(s[0]+s[1]));
+            return dst(generator) ? make_pair(1.0, make_tuple(s[0]+1,s[1],s[2],s[3])) :
+                                    make_pair(0.0, make_tuple(s[0],s[1]+1,s[2],s[3]));
+        }else if(a == 1){ // a == 1
+            bernoulli_distribution dst(s[2]/(s[2]+s[3]));
+            return dst(generator) ? make_pair(1.0, make_tuple(s[0],s[1],s[2]+1,s[3])) :
+                                    make_pair(0.0, make_tuple(s[0],s[1],s[2],s[3]+1));
+        }else{
+            throw InvalidOperationException("Unknown action");
+        }
+    }
+
+protected:
+    default_random_engine generator(seed);
+};
+
+
+/**
+Models the *real* multi-armed bandit. The probability of a success for each arm is
+independent from the state! It is given when the simulator is initialized. 
+
+State:
+( Arm0 positive count, Arm0 negative count, Arm1 positive count, Arm1 negative count )
+
+Action: {0,1} = arm to pull
+
+Careful with parallelization; shared random 
+*/
+class MultiArmedBandit {
+public:
+    typedef tuple<long,long,long,long> State;
+    typedef int Action; // TODO: change it to enum class?
+
+    /** Constructor
+    \param p0 Probability of success of arm 0
+    \param p1 Probability of success of arm 1 
+    */
+    MultiArmedBandit(double p0, double p1, random_device::result_type seed = random_device{}): 
+        generator(seed), p0(p0), p1(p1)
+    {
+        assert(p0 >= 0 && p0 <=1);
+        assert(p1 >= 0 && p1 <=1);
     };
 
-private:
-    
-    
+    /** The initial state in simulations */
+    State init_state(){return make_tuple(1,1,1,1)};
+
+    /** Probabilistic transition function */
+    pair<double,State> transition(State s, Action a){
+        assert(a == 0 || a == 1);
+
+        if(a == 0){
+            bernoulli_distribution dst(p0);
+            return dst(generator) ? make_pair(1.0, make_tuple(s[0]+1,s[1],s[2],s[3])) :
+                                    make_pair(0.0, make_tuple(s[0],s[1]+1,s[2],s[3]));
+        }else if(a == 1){ // a == 1
+            bernoulli_distribution dst(p1);
+            return dst(generator) ? make_pair(1.0, make_tuple(s[0],s[1],s[2]+1,s[3])) :
+                                    make_pair(0.0, make_tuple(s[0],s[1],s[2],s[3]+1));
+        }else{
+            throw InvalidOperationException("Unknown action");
+        }
+    }
+
+protected:
+    default_random_engine generator(seed);
+    double p0, p1;
 };
-}
+
+
+}}
--- src/algorithms/ThompsonSampling.cpp
+++ src/algorithms/ThompsonSampling.cpp
@@ -1,9 +1,14 @@
 #include "algorithms/ThompsonSampling.hpp"
+#include "domains/MultiArmBandit.hpp"
+
+namespace craam{
+namespace bandits{
 
 static double getRand(){
     return ((double) rand() / (RAND_MAX));
 }
 
+/** Samples a point from a beta distribution; the parameters are alpha and beta */
 double sampleFromBetaDist(double alpha, double beta){
     double randFromUnif = getRand();
     beta_distribution<> dist(alpha, beta);
@@ -12,54 +17,13 @@ double sampleFromBetaDist(double alpha, double beta){
     return randFromDist;
 }
 
-void applyThompsonSampling(){
-    cout<<"ThompsonSampling"<<endl;
+/** Thompson sampling policy */
+int thompson_sampling_policy(MultiArmedBandit::State state){
+    double betaSample0 = sampleFromBetaDist(state[0],state[1]);
+    double betaSample1 = sampleFromBetaDist(state[2],state[3]);
 
-    ofstream outfile;
-    outfile.open("ThompsonSampling_output.dat");
-
-    int state[4];
-    for(int i=0;i<4;i++){
-        //cout<<"i: "<<i<<endl;
-        state[i] = 1;
-    }
-
-    int numLevel=100;
-    for(int i=0;i<numLevel;i++){
-        //outfile<<"I: "<<i<<" --------------- "<<endl;
-        double betaSample1 = sampleFromBetaDist(state[0],state[1]);
-        double betaSample2 = sampleFromBetaDist(state[2],state[3]);
-        double r = getRand();
-
-        if(betaSample1>betaSample2){
-            //pull arm 1
-
-            double mean = ( (double)state[0]/( (double)state[0] + (double)state[1]));
-            //outfile<<"r: "<<r<<", mean: "<<mean<<endl;
-
-            if(r<mean){
-                //got a reward of 1 by pulling arm 1
-                state[0]++;
-                outfile<<"pull arm: 1, reward 1, mean: "<<mean<<endl;
-            } else{
-                //got a reward of 0 by pulling arm 1
-                state[1]++;
-                outfile<<"pull arm: 1, reward 0, mean: "<<mean<<endl;
-            }
-        } else{
-            double mean = ( (double)state[2]/( (double)state[2] + (double)state[3]));
-            //outfile<<"r: "<<r<<", mean: "<<mean<<endl;
-
-            if(r<mean){
-                //got a reward of 1 by pulling arm 2
-                state[2]++;
-                outfile<<"pull arm: 2, reward 1, mean: "<<mean<<endl;
-            } else{
-                //got a reward of 0 by pulling arm 2
-                state[3]++;
-                outfile<<"pull arm: 2, reward 0, mean: "<<mean<<endl;
-            }
-        }
-    }
+    return betaSample0 > betaSample1 ? 0 : 1;
 }
 
+// Run as:  simulate(MultiArmBandit(p0,p1), thompson_sampling_policy, horizon ...) see Simulation.hpp
+}}
